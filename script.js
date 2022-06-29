feaf821b16244b84c9a8b905ad68f80652e4a8ab
johanneswf <johanneswf@gmail.com>
2022-06-29 13:51:41 +0200

    MVP ugly

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-let str = 'strängen';
+const inputsubmit = document.querySelector('#inputsubmit');
+const btnsubmit = document.querySelector('#btnsubmit');
+const outputarea = document.querySelector('#output');
 
-str = 5;
+let uri = 'https://www.swapi.tech/api/people/?name=';
+
+async function getStarWars() {
+  // @ts-ignore
+  const fetched = await fetch(uri + inputsubmit.value)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+
+  let fetchlength = 0;
+  let outputresult = '';
+
+  if (fetched.result) fetchlength = fetched.result.length;
+
+  if (fetchlength > 0) {
+    for (let index = 0; index < fetchlength; index++) {
+      const props = fetched.result[index].properties;
+      outputresult += 
+
+`Name: ${props.name}
+Height: ${props.height}
+Gender: ${props.gender}
+Hair Color: ${props.hair_color}
+
+`; // Ugly
+
+    }
+    outputarea.innerHTML = outputresult;
+  }
+  else {
+    outputarea.innerHTML = 'No results';
+  }
+}
+
+btnsubmit.addEventListener('click', e => {
+  e.preventDefault;
+  getStarWars();
+})
+
+// height, mass, gender och hair_color
