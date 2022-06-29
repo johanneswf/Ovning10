3358a6003c75d08d6c9f1550af9e88b605ae957c
johanneswf <johanneswf@gmail.com>
2022-06-29 15:18:38 +0200

    Cleanup

--- Uppgift 1/script.js
+++ Uppgift 1/script.js
@@ -5,39 +5,34 @@ const outputarea = document.querySelector('#output');
 const uri = 'https://www.swapi.tech/api/people/?name=';
 
 const getStarWars = async function () {
-  // @ts-ignore
-  const fetched = await fetch(uri + '' + inputsubmit.value)
-    .then(res => res.json())
-    .catch(err => console.log(err));
-
-  let fetchlength = 0;
   let outputresult = '';
 
-  if (fetched.result) fetchlength = fetched.result.length;
-
-  if (fetchlength > 0) {
-    for (let index = 0; index < fetchlength; index++) {
-      const props = fetched.result[index].properties;
-      outputresult += 
-
+  if (inputsubmit.value) {
+    const fetched = await fetch(uri + '' + inputsubmit.value)
+      .then(res => res.json())
+      .then(data => {
+        if (data.result.length > 0) {
+          for (let index = 0; index < data.result.length; index++) {
+            const props = data.result[index].properties;
+            outputresult +=
+// Ugly
 `Name: ${props.name}
 Height: ${props.height}
 Gender: ${props.gender}
 Hair Color: ${props.hair_color}
 
-`; // Ugly
-
-    }
-    outputarea.innerHTML = outputresult;
+`;
+          }
+          outputarea.innerHTML = outputresult;
+        }
+        else outputarea.innerHTML = 'No results.';
+      })
+      .catch(err => console.log(err));
   }
-  else {
-    outputarea.innerHTML = 'No results';
-  }
+  else outputarea.innerHTML = 'Nothing submitted.';
 }
 
 btnsubmit.addEventListener('click', e => {
   e.preventDefault;
   getStarWars();
 })
-
-// height, mass, gender och hair_color
