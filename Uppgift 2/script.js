9006801d0261ee8f40bbdf748f6e626d3298faa9
johanneswf <johanneswf@gmail.com>
2022-06-29 14:39:28 +0200

    MVP

--- Uppgift 2/script.js
+++ Uppgift 2/script.js
@@ -8,12 +8,13 @@ async function getCard() {
       const imgcard = document.createElement('img');
       imgcard.setAttribute("src", data.cards[0].image);
 
+      divcard.innerHTML = '';
       divcard.appendChild(imgcard);
-    });
+    })
+    .catch(err => console.log(err));
 }
 
 btndraw.addEventListener('click', function (e) {
   e.preventDefault();
-  divcard.innerHTML = '';
   getCard();
 })
